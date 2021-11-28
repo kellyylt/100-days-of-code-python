ce0623e30f223558d1c9f2b2cc9950644cb9ba39
Kelly Tan <kellyylt@hotmail.com>
2021-11-28 20:59:09 +0800

    Day 1 : Band Name Generator

---
+++ day-01.py
@@ -0,0 +1,5 @@
+#Suggest band name based on city and pet name
+print("Hello there! Welcome to the band name generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name is " + city + " " + pet)
