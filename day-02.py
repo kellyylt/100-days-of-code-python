53ab97f0aff5871be68f4608d3dd64a96cf65b0c
Kelly Tan <kellyylt@hotmail.com>
2021-11-29 21:39:10 +0800

    Day 2 : Tip Calculator

--- day-02.py
+++ day-02.py
@@ -5,5 +5,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
 people = int(input("How many people to split the bill? "))
 to_pay_per_person = round(bill * (1 + tip / 100) / people, 2)
+final_amount = "{:.2f}".format(to_pay_per_person)
 
-print(f"Each person should pay ${to_pay_per_person}")
+print(f"Each person should pay ${final_amount}")
