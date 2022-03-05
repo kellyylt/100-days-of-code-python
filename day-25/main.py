df513247a1b2c66fb3567e15ae6baa1780524d5f
Kelly Tan <kellyylt@hotmail.com>
2022-03-05 13:14:20 +0800

    Day 26 : NATO Alphabet Project

--- day-25/main.py
+++ day-25/main.py
@@ -19,10 +19,7 @@ while len(guessed_state) <= 50:
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 correct", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in states_list["state"].to_list():
-            if state not in guessed_state:
-                missing_states.append(state)
+        missing_states = [state for state in states_list["state"].to_list() if state not in guessed_state]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("100-days-of-code-python/day-25/states_to_learn.csv")
         break
