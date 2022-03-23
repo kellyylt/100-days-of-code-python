a5b4977c84dad55379d3ed5485bef0134f59ff1b
Kelly Tan <kellyylt@hotmail.com>
2022-03-23 14:51:16 +0800

    Day 26 : NATO Alphabet Project

--- day-26/main.py
+++ day-26/main.py
@@ -1,15 +1,20 @@
 import pandas
 
 #Read in dataframe
-nato_df = pandas.read_csv("100-days-of-code-python/day-26/nato_phonetic_alphabet.csv")
+nato_df = pandas.read_csv("day-26/nato_phonetic_alphabet.csv")
 
 #Dataframe to dictionary
 nato_dic = {row.letter: row.code for (index,row) in nato_df.iterrows()}
 
-#Prompt user input
-user_input = input("Enter a word: ").upper()
-
 #Create list
-nato_list = [nato_dic[letter] for letter in user_input]
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        nato_list = [nato_dic[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_list)
 
-print(nato_list)
+generate_phonetic()
